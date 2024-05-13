234762cba88a484fd0768575b223fa16d19d61b5
Mishal Aslam <mishalaslam17@gmail.com>
2024-05-13 16:36:19 +0500

    ...

--- src/Components/LatestJewelry.jsx
+++ src/Components/LatestJewelry.jsx
@@ -8,7 +8,7 @@ const LatestJewelry = () => {
         <h2 className="text-[#002D69] my-8 text-3xl font-bold text-left  uppercase">
           our latest jewelry
         </h2>
-
+<h1>fvdfvdvdfv</h1>
         <div className="lg:grid-cols-3 grid md:grid-cols-2 grid-cols-1  gap-5 ">
           <div>
             <img
@@ -31,14 +31,11 @@ const LatestJewelry = () => {
               className="md:h-[400px] h-[300px] md:w-[400px]  w-full"
             />
           </div>
-
         </div>
 
-
-
-          <button className="bg-[#002D69] hover:bg-[#1A3F6F] text-white font-bold py-2 px-6 rounded hover:bg-transparent  hover:text-[#002D69] hover:border-[#002D69] border transition duration-700 ease-in-out flex justify-center items-center m-auto my-12">
-            View Gallery
-          </button>
+        <button className="bg-[#002D69] hover:bg-[#1A3F6F] text-white font-bold py-2 px-6 rounded hover:bg-transparent  hover:text-[#002D69] hover:border-[#002D69] border transition duration-700 ease-in-out flex justify-center items-center m-auto my-12">
+          View Gallery
+        </button>
       </div>
     </div>
   );
